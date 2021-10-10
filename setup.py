6dfa5ae5f99f0640678d499ed6068d0cf6df763b
daniel <danieljschutz@gmail.com>
2021-10-10 09:03:45 -0400

    updated _ to - in name

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="streamlit_heroku",
+    name="streamlit-heroku",
     version="0.0.1",
     author="Daniel Schutz",
     author_email="danieljschutz@gmail.com",
